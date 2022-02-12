5af5dfab668a711153944eca390fba13c3f88133
Marcos <marcospaulo.mpf34@gmail.com>
2022-02-11 23:36:55 -0300

    Iniciando o desafio do FrontEnd Mentor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <img src='./logo.svg' alt='logo'></img>
+
+        <div className="score">
+          <p>SCORE</p>
+          <span>12</span>
+        </div>
       </header>
+
+      <main className="play">
+
+        <button className="choice-btn" data-choice="paper">
+          <div className="choice paper">
+            <img src='./icon-paper.svg' alt='icon-paper'></img>
+          </div>
+        </button>
+
+        <button className="choice-btn" data-choice="scissors">
+          <div className="choice scissors">
+            <img src='./icon-scissors.svg' alt='icon-scissors'></img>
+          </div>
+        </button>
+
+        <button className="choice-btn" data-choice="rock">
+          <div className="choice rock">
+            <img src='./icon-rock.svg' alt='icon-rock'></img>
+          </div>
+        </button>
+      </main>
+
+      Rules
+
+      You Picked
+      The House Picked
+
+      You Win
+      You Lose
+
+      Play Again
     </div>
   );
 }
